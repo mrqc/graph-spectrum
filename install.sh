ab37f7079579702f0d9798c44610f4c5200676a6
Erhard Dinhobl <er@master-studios.net>
2018-03-30 14:40:20 +0200

    info

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #if you would like to use the scripts, execute the following commands on your unix machine
 # and of course you need python
-´´´
 sudo pip install pandas
 sudo pip install networkx
 sudo pip install matplotlib
-´´´
+sudo pip install numpy
