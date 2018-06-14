7db229f17af6c63388404d6259b3179c5b923cec
Erhard Dinhobl <er@master-studios.net>
2018-06-14 07:53:36 +0200

    - correct calculation of D

--- graphfeature.py
+++ graphfeature.py
@@ -1,11 +1,19 @@
 import graphgenerate
 import numpy
 
-def degrees(adjMat, nodeIndex):
-  D_in = adjMat[nodeIndex]
-  D_out = numpy.transpose(adjMat)[nodeIndex]
-  return D_in, D_out, numpy.sum(D_in), numpy.sum(D_out)
+def degrees(adjMat):
+  D_in = numpy.zeros((len(adjMat), len(adjMat)))
+  D_out = numpy.zeros((len(adjMat), len(adjMat)))
+  for nodeIndex in range(0, len(adjMat)):
+    adjV1 = adjMat[nodeIndex]
+    adjV2 = numpy.transpose(adjMat)[nodeIndex]
+    D_in[nodeIndex][nodeIndex] = numpy.sum(adjV1)
+    D_out[nodeIndex][nodeIndex] = numpy.sum(adjV2)
+  D = numpy.add(D_in, D_out)
+  return D_in, D_out, D
 
 adjMat = numpy.fromfunction(graphgenerate.adjFun, (50, 50))
-(D_in, D_out, d_in, d_out) = degrees(adjMat, 2)
-print D_in, D_out, d_in, d_out
+(D_in, D_out, D) = degrees(adjMat)
+print D_in
+print D_out
+print D
