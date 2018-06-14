4f11622b8fb65791db75059da483a2b45e3d0f5d
Erhard Dinhobl <er@master-studios.net>
2018-06-14 10:15:41 +0200

    new values

--- graphfeature.py
+++ graphfeature.py
@@ -4,16 +4,21 @@ import numpy
 def degrees(adjMat):
   D_in = numpy.zeros((len(adjMat), len(adjMat)))
   D_out = numpy.zeros((len(adjMat), len(adjMat)))
+  I = numpy.identity(len(adjMat))
   for nodeIndex in range(0, len(adjMat)):
     adjV1 = adjMat[nodeIndex]
     adjV2 = numpy.transpose(adjMat)[nodeIndex]
     D_in[nodeIndex][nodeIndex] = numpy.sum(adjV1)
     D_out[nodeIndex][nodeIndex] = numpy.sum(adjV2)
   D = numpy.add(D_in, D_out)
-  return D_in, D_out, D
+  selfLoopMatrix = numpy.multiply(adjMat, I)
+  D_complete = numpy.subtract(D, selfLoopMatrix)
+  return D_in, D_out, D, D_complete
 
 adjMat = numpy.fromfunction(graphgenerate.adjFun, (50, 50))
-(D_in, D_out, D) = degrees(adjMat)
-print D_in
-print D_out
-print D
+(D_in, D_out, D, D_complete) = degrees(adjMat)
+print adjMat # adjancency matrix
+print D_in # diagonal degree matrix for incoming edges
+print D_out # diagonal degree matrix for outgoing edges
+print D # diagonal degree matrix D_in + D_out
+print D_complete # diagonal degree matrix excluding self loops
