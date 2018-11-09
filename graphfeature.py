f6a8d0cdbe04648eb8188ceb2d5bee4f70f9117d
Erhard Dinhobl <er@master-studios.net>
2018-11-09 14:08:50 +0100

    new features

--- graphfeature.py
+++ graphfeature.py
@@ -1,5 +1,6 @@
 import graphgenerate
 import numpy
+import pprint
 
 def degrees(adjMat):
   D_in = numpy.zeros((len(adjMat), len(adjMat)))
@@ -34,12 +35,10 @@ def getUndirectedAdj(directedAdjMat):
 def generateFeatureMatrix(adjMat, D):
   matrix = []
   for nodeIndex in range(0, len(adjMat)):
+    print "processing node " + str(nodeIndex)
     processedNodes = []
-    featureVector = getFeatureVectorOfNode(adjMat, D, nodeIndex, 0,  processedNodes)
-    if featureVector == None:
-      matrix.append([])
-    else:
-      matrix.append(featureVector)
+    featureVector = getFeatureMatrixOfNode(adjMat, D, nodeIndex, 0,  processedNodes)
+    matrix.append(featureVector)
   return matrix
 
 #  for index1 in range(0, len(adjMat)):
@@ -55,21 +54,25 @@ def generateFeatureMatrix(adjMat, D):
 #      featureMatrix.append(vectorPreviousToAppend)
 #    yield featureMatrix
 
-def getFeatureVectorOfNode(adjMat, D, index, depth, processedNodes):
-  if depth > 4: # or index in processedNodes:
+maxDepth = 2
+
+def getFeatureMatrixOfNode(adjMat, D, index, depth, processedNodes):
+  if depth > maxDepth:# or index in processedNodes:
+    print "not performing " + str(index) + " in " + str(depth)
     return None
+  print "features of node index: " + str(index) + " in depth " + str(depth)
   adjNodes = getAdjacentNode(adjMat, index)
+  print adjNodes
   degreeNode = getDegreeNode(D, index)
   processedNodes.append(index)
+  matrix = [degreeNode]
   for indexAdj in range(0, len(adjNodes)):
-    newNodeDegree = getFeatureVectorOfNode(adjMat, D, adjNodes[indexAdj], depth + 1, processedNodes)
+    newNodeDegree = getFeatureMatrixOfNode(adjMat, D, adjNodes[indexAdj], depth + 1, processedNodes)
     if newNodeDegree != None:
-      return [degreeNode] + newNodeDegree
-    else:
-      return [degreeNode]
+      matrix.append(newNodeDegree)
+  return matrix
 
 if __name__ == "__main__":
-  #adjMat_dir, D_in, D_out, D, adjMat_undir = graphgenerate.generateRandomGraph(4, 4)
   adjMat_dir = [[1, 0, 0, 1,],
                 [0, 0, 0, 0,], 
                 [0, 0, 0, 0,],
@@ -90,10 +93,11 @@ if __name__ == "__main__":
                   [0, 0, 0, 0,],
                   [0, 0, 0, 0,],
                   [1, 0, 0, 0,]]
+  #adjMat_dir, D_in, D_out, D, adjMat_undir = graphgenerate.generateRandomGraph(4, 4)
   #print "adjMat_dir\n", adjMat_dir # adjancency matrix
   #print "D_in\n", D_in # diagonal degree matrix for incoming edges
   #print "D_out\n", D_out # diagonal degree matrix for outgoing edges
   #print "D\n", D # diagonal degree matrix D_in + D_out
-  #print "adjMat_undir\n", adjMat_undir
+  print "adjMat_undir\n", adjMat_undir
 
-  print generateFeatureMatrix(adjMat_undir, D)
+  pprint.PrettyPrinter(indent=4).pprint(generateFeatureMatrix(adjMat_undir, D))
