b110ff14e84bac3593a0bd02b734735075b0379c
Erhard Dinhobl <er@master-studios.net>
2019-01-25 15:16:00 +0100

    directed graphs

--- generate-and-display-random-graph.py
+++ generate-and-display-random-graph.py
@@ -10,7 +10,9 @@ import graphkstest
 from scipy.spatial import distance
 from scipy import stats
 import matplotlib.cm as cm
-
+import warnings
+import sys
+warnings.simplefilter("ignore")
 nodeCount = 10
 
 def sqr(v1, v2):
@@ -21,14 +23,34 @@ def sqr(v1, v2):
     error = error + numpy.power(numpy.absolute(v1[index] - v2[index]), graphfeature.maxDepth - index + 1)
   return error # / float(len(v1))
 
+def ppArray(a, depth=0):
+  for ele in a:
+    if isinstance(ele, list):
+      if isinstance(ele[0], list):
+        print ' ' * (depth * 3), "["
+        ppArray(ele, depth + 1)
+        print ' ' * (depth * 3), "]"
+      else:
+        print ' ' * (depth * 3), str(ele)
+    else:
+      print ' ' * (depth * 3), str(ele) + ","
+
 def comparePart(node1Features, node2Features):
   indexesUsed = []
   bestIndexes = []
   for index1 in range(0, len(node1Features)):
     dists = []
     for index2 in range(0, len(node2Features)):
+      #print node1Features[index1]
+      #print node2Features[index2]
       v1 = numpy.log(node1Features[index1])
       v2 = numpy.log(node2Features[index2])
+      for index in range(0, len(v1)):
+        if v1[index] == numpy.NINF:
+          v1[index] = 0
+      for index in range(0, len(v2)):
+        if v2[index] == numpy.NINF:
+          v2[index] = 0
       test = sqr(v1, v2)
       dist = test
       dists.append(dist)
@@ -51,14 +73,68 @@ def compare(node1Features, node2Features):
       return ([(0.0, 0.0)], numpy.inf)
   elif node2Features[0][0] == 0:
     return ([(0.0, 0.0)], numpy.inf)
-  indexes1 = comparePart(node1Features, node2Features)
-  indexes2 = comparePart(node2Features, node1Features)
+  length = len(node1Features[0]) if len(node1Features[0]) < len(node2Features[0]) else len(node2Features[0])
+  node1FeaturesLocal = node1Features
+  node2FeaturesLocal = node2Features
+  for index in range(0, len(node1FeaturesLocal)):
+    node1FeaturesLocal[index] = node1FeaturesLocal[index][0:length]
+  for index in range(0, len(node2FeaturesLocal)):
+    node2FeaturesLocal[index] = node2FeaturesLocal[index][0:length]
+  indexes1 = comparePart(node1FeaturesLocal, node2FeaturesLocal)
+  indexes2 = comparePart(node2FeaturesLocal, node1FeaturesLocal)
   diff1 = diff(indexes1)
   diff2 = diff(indexes2)
   return (indexes1, diff1) if diff1 < diff2 else (indexes2, diff2)
 
+def diff(v):
+  diffVal = 0
+  for data in v:
+    diffVal = diffVal + numpy.power(data[2], 2)
+  diffVal = diffVal / len(v)
+  return diffVal
+
 def euclideanDist(v1, v2):
   return distance.euclidean(v1, v2)
+
+def printPairs(pairs):
+  for pair in pairs:
+    if pair[0] != pair[1]:
+      print pair[0], "vs.", pair[1], "=", pair[2]
+
+def calcDiffs(adjMat, DegMat):
+  featureMatrix = graphfeature.generateFeatureMatrix(adjMat, DegMat)
+  finishedMatrix = []
+  for featureIndex in range(0, len(featureMatrix)):
+    finishedMatrix.append([])
+    matrix = []
+    graphfeature.processMatrix(featureMatrix[featureIndex], [], matrix)
+    length = 0
+    for vec in matrix:
+      if len(vec) > length:
+        length = len(vec)
+
+    for vec in matrix:
+      if len(vec) == length:
+        finishedMatrix[len(finishedMatrix) - 1].append(vec)
+      elif len(vec) == 1:
+        if vec[0] == 0:
+          finishedMatrix[len(finishedMatrix) - 1].append([0])
+  normalizedMatrix = []
+  for matrix in finishedMatrix:
+    normalizedMatrix.append([])
+    for vector in matrix:
+      normalizedMatrix[len(normalizedMatrix) - 1].append(vector)
+
+  pairs = []
+  keyPairs = {}
+  for index1 in range(0, len(normalizedMatrix)):
+    for index2 in range(index1 + 1, len(normalizedMatrix)):
+      cVal = compare(normalizedMatrix[index1], normalizedMatrix[index2])
+      pairs.append((index1, index2, cVal[1]))
+      keyPairs[(index1, index2)] = cVal[1]
+
+  pairs.sort(key=lambda x: x[2], reverse=False)
+  return pairs, keyPairs
 
 if __name__ == "__main__":
   adjMat_dir, D_in, D_out, D, adjMat_undir = graphgenerate.generateRandomGraph(nodeCount, nodeCount)
@@ -126,52 +202,34 @@ if __name__ == "__main__":
 #    [0, 0, 0, 0],
 #    [0, 0, 0, 0],
 #    [0, 0, 0, 0]])
-
   plt.subplot(221)
+  graphdisplay.renderDiGraphFromAdj(adjMat_dir)
+  plt.subplot(222)
   graphdisplay.renderGraphFromAdj(adjMat_undir)
-  featureMatrix = graphfeature.generateFeatureMatrix(adjMat_undir, D)
-  finishedMatrix = []
-  for featureIndex in range(0, len(featureMatrix)):
-    finishedMatrix.append([])
-    matrix = []
-    graphfeature.processMatrix(featureMatrix[featureIndex], [], matrix)
-    length = 0
-    for vec in matrix:
-      if len(vec) > length:
-        length = len(vec)
+  #undirected graph
+  pairs1, keyPairs1 = calcDiffs(adjMat_undir, D)
+  print "Undirected Graph Differences between nodes"
+  printPairs(pairs1)
 
-    for vec in matrix:
-      if len(vec) == length:
-        finishedMatrix[len(finishedMatrix) - 1].append(vec)
-      elif len(vec) == 1:
-        if vec[0] == 0:
-          finishedMatrix[len(finishedMatrix) - 1].append([0])
-
-  normalizedMatrix = []
-  for matrix in finishedMatrix:
-    normalizedMatrix.append([])
-    for vector in matrix:
-      normalizedMatrix[len(normalizedMatrix) - 1].append(vector)
-
-  def diff(v):
-    diffVal = 0
-    for data in v:
-      diffVal = diffVal + numpy.power(data[2], 2)
-    diffVal = diffVal / len(v)
-    return diffVal
-
-  pairs = []
-  for index1 in range(0, len(normalizedMatrix)):
-    for index2 in range(index1 + 1, len(normalizedMatrix)):
-      cVal = compare(normalizedMatrix[index1], normalizedMatrix[index2])
-      pairs.append((index1, index2, cVal[1]))
-
-  pairs.sort(key=lambda x: x[2], reverse=False)
-  for pair in pairs:
-    if pair[0] != pair[1]:
-      print pair[0], "vs.", pair[1], "=", pair[2]
-    if pair[2] > 2:
-      break
-
-  plt.show()
+  #directed graph
+  pairs2, keyPairs2 = calcDiffs(adjMat_dir, D_out)
+  pairs3, keyPairs3 = calcDiffs(adjMat_dir, D_in)
+  pairs4 = []
+  #printPairs(pairs2)
+  #print "---"
+  #printPairs(pairs3)
+  #for ele2 in pairs2:
+  #  cVal1 = ele2[2]
+  #  cVal2 = keyPairs3[(ele2[0], ele2[1])]
+  #  if cVal1 != numpy.Inf and cVal2 != numpy.Inf:
+  #    pairs4.append((ele2[0], ele2[1], cVal1, cVal2))
+  #print "Directed Graph Differences between nodes"
+  #for index1 in range(0, len(pairs4)):
+  #  for index2 in range(0, len(pairs4)):
+  #    x1 = pairs4[index1][2]
+  #    y1 = pairs4[index1][3]
+  #    x2 = pairs4[index2][2]
+  #    y2 = pairs4[index2][3]
+  #    print pairs4[index1][euclideanDist([x1, y1], [x2, y2])
+  #plt.show()
 
--- graphdisplay.py
+++ graphdisplay.py
@@ -6,18 +6,21 @@ import numpy as np
 
 def renderDiGraphFromAdj(adjMat):
   graph = nx.DiGraph(adjMat)
-  pos = nx.spring_layout(graph)
+  pos = nx.circular_layout(graph)
   #nx.draw(graph, cmap = plt.get_cmap('jet'))
-  nx.draw_networkx_nodes(graph, pos, nodelist=graph.nodes, ax=None)
+  nx.draw_networkx_nodes(graph, pos, nodelist=graph.nodes, ax=None, node_size=400)
   nx.draw_networkx_edges(graph, pos, edgelist=graph.edges, width=1.0, alpha=0.5)
   labels = {}
   for index in range(0, len(adjMat)):
-    labels[index] = "___" + str(index)
+    labels[index] = str(index)
   nx.draw_networkx_labels(graph, pos, labels)
 
 def renderGraphFromAdj(adjMat):
   graph = nx.Graph(adjMat)
-  pos = nx.spring_layout(graph, k=400)
+  #pos = nx.spring_layout(graph, k=400)
+  #pos = nx.shell_layout(graph)
+  #pos = nx.spectral_layout(graph)
+  pos = nx.circular_layout(graph)
   #nx.draw(graph, cmap = plt.get_cmap('jet'))
   #nx.draw_networkx_nodes(graph, pos, nodelist=graph.nodes, node_size=np.empty(len(graph.nodes)).fill(50))
   nx.draw_networkx_nodes(graph, pos, nodelist=graph.nodes, node_size=400)
